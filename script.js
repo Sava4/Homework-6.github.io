973e9d807ab86e93d51d2addd55dbaa5c8e7113f
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-05-26 15:36:45 +0300

    all done

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert('Hello world!');
+let lastAge = 'unmodified'; 
+let lastName = 'unmodified';
+
+let age = prompt('How old are you?');
+
+while (age < 1 || !Number.isInteger(parseInt(age))) {
+    lastAge = age;
+    age = prompt('How old are you? Please enter age number', `${lastAge}`);
+}
+
+let name = prompt('Please enter your name');
+
+while (name === null || name === '' || name === ' ' || !isNaN(name)) {
+    lastName = name;
+    name = prompt('Please enter your name', `${lastName}`);
+}
+
+if (age < 18) {
+    alert('You are not allowed to visit this website.');
+} else if (age <= 22) {
+    let decision = confirm('Are you sure you want to continue?');
+    decision ? alert(`Welcome, ${name}`) : alert('You are not allowed to visit this website.');
+} else if (age > 22) {
+    alert(`Welcome, ${name}`);
+}
